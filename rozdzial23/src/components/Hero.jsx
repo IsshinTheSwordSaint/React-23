f5af196cfc4badea6d4d3e1e560d042dcd1662cb
IsshinTheSwordSaint <ut_4003@zsp3zamosc.pl>
2026-06-01 11:17:30 +0200

    Add files via upload

---
+++ rozdzial23/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+function Hero() {
+  return (
+    <>
+      <section className="bg-gradient-to-r from-blue-500 to-purple-600 min-h-screen md:min-h-screen flex items-center justify-center px-6">
+        <div className="text-center text-white max-w-2xl">
+          <h1 className="text-3xl md:text-6xl font-bold mb-4">Cyganejro</h1>
+
+          <p className="text-base md:text-xl mb-6">
+            Opis tekstu es es cygan gemini pro pro pro wiesz ocb jest jak jest
+            mordo
+          </p>
+
+          <button className="bg-white text-black px-6 py-3 rounded-lg font-semibold hover:bg-gray-200 transition">
+            klik klip pą pą pą
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
